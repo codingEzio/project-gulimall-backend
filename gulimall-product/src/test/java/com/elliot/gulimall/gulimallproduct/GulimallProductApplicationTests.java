5b0ab22140b655e2d46fefc8d29911a529b1d6ee
!Mensch <assassinste@gmail.com>
2022-10-04 23:41:40 +0800

    dev: Test if component 'gulimall-product' works (with generated code)

--- gulimall-product/src/test/java/com/elliot/gulimall/gulimallproduct/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/elliot/gulimall/gulimallproduct/GulimallProductApplicationTests.java
@@ -1,13 +1,26 @@
 package com.elliot.gulimall.gulimallproduct;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.elliot.gulimall.gulimallproduct.entity.BrandEntity;
+import com.elliot.gulimall.gulimallproduct.service.BrandService;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+	@Autowired
+	BrandService brandService;
+
 	@Test
 	void contextLoads() {
+		BrandEntity brandEntity = new BrandEntity();
+
+		brandEntity.setName("ok");
+		brandService.save(brandEntity);
+
+		System.out.println("Saved!");
 	}
 
 }
