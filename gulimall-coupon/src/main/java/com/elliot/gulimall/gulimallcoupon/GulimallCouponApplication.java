54ea2ad1c40b8d1d0d29c51310f95a35eb643769
!Mensch <assassinste@gmail.com>
2022-10-04 20:05:15 +0800

    dev: Database configuration for gulimall-product

--- gulimall-coupon/src/main/java/com/elliot/gulimall/gulimallcoupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/elliot/gulimall/gulimallcoupon/GulimallCouponApplication.java
@@ -1,8 +1,10 @@
 package com.elliot.gulimall.gulimallcoupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.elliot.gulimall.gulimallcoupon.dao")
 @SpringBootApplication
 public class GulimallCouponApplication {
 
