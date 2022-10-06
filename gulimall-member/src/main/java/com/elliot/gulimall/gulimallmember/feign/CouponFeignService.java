8485fd204c3dbb555624eac02bdc60f6acc8fc3a
!Mensch <assassinste@gmail.com>
2022-10-06 21:47:06 +0800

    dev: Renamed each components' app name for clarify

--- gulimall-member/src/main/java/com/elliot/gulimall/gulimallmember/feign/CouponFeignService.java
+++ gulimall-member/src/main/java/com/elliot/gulimall/gulimallmember/feign/CouponFeignService.java
@@ -2,10 +2,9 @@ package com.elliot.gulimall.gulimallmember.feign;
 
 import com.elliot.common.utils.R;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("gulimallcoupon")
+@FeignClient("nacos-gulimall-coupon")
 public interface CouponFeignService {
 
 	@RequestMapping("/gulimallcoupon/coupon/member/list")
